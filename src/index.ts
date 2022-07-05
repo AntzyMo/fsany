dcd768c1ea2267619949386af649ad9fff49615f
AntzyMo <mozbano@163.com>
2022-07-05 21:26:46 +0800

    feat: copy move remove

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,27 @@
 import { chdir } from 'process'
 import { promises as fs } from 'fs'
 import { resolve } from 'path'
-import { outputFile } from 'fs-extra'
+import { outputFile, remove as fsRemove, copy as fsCopy, move as fsMove } from 'fs-extra'
+
+type stringOrArray=string[] | string
 
 export const getexport = () => {
   return fs
 }
 
-export const initialPath = (path:string[] | string) => {
+export const initialPath = (path:stringOrArray) => {
   const handlePath = typeof path === 'string' ? [path] : path
   return resolve(...handlePath)
 }
 
 // write file
-export const writeFile = async (file:string[]| string, data:string) => {
+export const writeFile = async (file:stringOrArray, data:string) => {
   const path = initialPath(file)
   await outputFile(path, data)
 }
 
 // read file or Dir
-export const read = async (file:string[]| string) => {
+export const read = async (file:stringOrArray) => {
   const path = initialPath(file)
   const isFile = path.includes('.')
   if (isFile) {
@@ -27,6 +29,23 @@ export const read = async (file:string[]| string) => {
   } else {
     return await fs.readdir(path, { encoding: 'utf-8' })
   }
+}
+
+export const copy = async (src:stringOrArray, dest:stringOrArray) => {
+  const path = initialPath(src)
+  const destPath = initialPath(dest)
+  await fsCopy(path, destPath)
+}
+
+export const move = async (src:stringOrArray, dest:stringOrArray) => {
+  const path = initialPath(src)
+  const destPath = initialPath(dest)
+  await fsMove(path, destPath)
+}
+
+export const remove = async (path:stringOrArray) => {
+  const handlePath = initialPath(path)
+  await fsRemove(handlePath)
 }
 
 // 进入目录
